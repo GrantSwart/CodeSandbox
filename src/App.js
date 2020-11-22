f6cd4faa7a3b475cf4174a8bb8cc245b25927eaf
Grant Swart <grant@lite-support.co.uk>
2020-11-22 21:16:21 +0000

    Main components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./styles.css";
 
+import Header from "/Header";
+import Meme from "/Meme";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandboxTest</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <Header />
+        <Meme />
+      </div>
     </div>
   );
 }
